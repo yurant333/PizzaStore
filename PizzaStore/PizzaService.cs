f8b8dbb9d389837d09b4910c2dcc9e551a166722
yurant333 <yurant333@gmail.com>
2024-08-22 07:29:05 +0400

    add pizza service

---
+++ PizzaStore/PizzaService.cs
@@ -0,0 +1,30 @@
+﻿using PizzaStore.Models;
+    
+namespace PizzaStore.Service;
+
+public class PizzaService
+{
+    private readonly List<Pizza> _pizzas = new();
+
+    public IEnumerable<Pizza> GetAllPizzas()
+    {
+        return _pizzas;
+    }
+
+    public Pizza AddPizza(string name, string description)
+    {
+        var newPizza = new Pizza
+        {
+            Id = _pizzas.Count + 1,
+            Name = name,
+            Description = description
+        };
+        _pizzas.Add(newPizza);
+        return newPizza;
+    }
+
+    public Pizza? GetPizzaById(int id)
+    {
+        return _pizzas.FirstOrDefault(p => p.Id == id);
+    }
+}
